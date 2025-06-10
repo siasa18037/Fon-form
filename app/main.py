74e2368145fa9da6751fa6c9c56f88a8922876c9
Siasa <84365286+siasa18037@users.noreply.github.com>
2025-06-10 07:50:28 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from . import models, schemas, crud
 from .database import SessionLocal, engine, Base
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi import HTTPException
 
 # สร้างตารางในฐานข้อมูล SQLite
 Base.metadata.create_all(bind=engine)
@@ -24,10 +25,17 @@ def get_db():
     finally:
         db.close()
 
-@app.post("/patients/", response_model=schemas.Patient)
+@app.post("/api/form/", response_model=schemas.Patient)
 def create_patient(patient: schemas.PatientCreate, db: Session = Depends(get_db)):
     return crud.create_patient(db=db, patient=patient)
 
-@app.get("/patients/", response_model=list[schemas.Patient])
+@app.get("/api/form/", response_model=list[schemas.Patient])
 def read_patients(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     return crud.get_patients(db=db, skip=skip, limit=limit)
+
+@app.delete("/api/form/{patient_id}", response_model=schemas.Patient)
+def delete_patient(patient_id: int, db: Session = Depends(get_db)):
+    deleted = crud.delete_patient(db, patient_id)
+    if deleted is None:
+        raise HTTPException(status_code=404, detail="ไม่พบผู้ป่วยที่ต้องการลบ")
+    return deleted
